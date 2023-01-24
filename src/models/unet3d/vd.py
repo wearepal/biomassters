ef8aea74a7b02bfb890f38ef2e8a5320a878964e
MylesBartlett <mb715@sussex.ac.uk>
2023-01-24 15:22:19 +0000

    Add dropout to spatial attention layers.

--- src/models/unet3d/vd.py
+++ src/models/unet3d/vd.py
@@ -184,12 +184,17 @@ class ResnetBlock3d(nn.Module):
 
 
 class SpatialLinearAttention(nn.Module):
-    def __init__(self, dim: int, *, heads: int = 4, dim_head: int = 32) -> None:
+    def __init__(
+        self, dim: int, *, heads: int = 4, dim_head: int = 32, dropout: float = 0.05
+    ) -> None:
         super().__init__()
         self.scale = dim_head**-0.5
         self.heads = heads
         hidden_dim = dim_head * heads
-        self.to_qkv = nn.Conv2d(dim, out_channels=hidden_dim * 3, kernel_size=1, bias=False)
+        self.to_qkv = nn.Sequential(
+            nn.Dropout(p=dropout),
+            nn.Conv2d(dim, out_channels=hidden_dim * 3, kernel_size=1, bias=False),
+        )
         self.to_out = nn.Conv2d(hidden_dim, out_channels=dim, kernel_size=1)
 
     @override
